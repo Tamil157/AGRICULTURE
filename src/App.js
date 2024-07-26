2ad9c65b865638c12db918c85d5ab4b691adffd3
Vignesh-kumar-k <vigneshvicky84321@gmail.com>
2024-07-26 19:13:38 +0530

    Updated services

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter,Routes,Route,Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./Pages/About";
 import Services from "./Pages/Services";
+import Crop_recommend from "./Pages/Crop_recommend";
 
 export default function App(){
   return(
@@ -12,6 +13,7 @@ export default function App(){
         <Route path='/' element={<Home />}/>
         <Route path='/about' element={<About />}/>
         <Route path='/services' element={<Services />}/>
+        <Route path='/crop_recommend' element={<Crop_recommend />}/>
       </Routes>
       </BrowserRouter>
 
