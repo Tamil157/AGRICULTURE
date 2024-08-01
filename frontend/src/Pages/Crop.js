3f452942135ecfd43fb135f68cd758857202922b
TAMILARASAN D <144335798+Tamil157@users.noreply.github.com>
2024-08-01 18:31:06 +0530

    Update Crop.js

--- frontend/src/Pages/Crop.js
+++ frontend/src/Pages/Crop.js
@@ -11,7 +11,7 @@ const Crop = () => {
     event.preventDefault();
     const fileInput = document.querySelector('input[type="file"]');
     const formData = new FormData();
-    formData.Cropend('file', fileInput.files[0]);
+    formData.append('file', fileInput.files[0]); // Corrected method name
 
     try {
       const response = await axios.post('http://127.0.0.1:5000/predict', formData, {
